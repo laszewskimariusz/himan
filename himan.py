fe5136bc657846a7edd3a32462598ab79659989d
Mariusz Laszewski <laszewskimariusz@gmail.com>
2015-06-10 20:08:12 +0100

    Operation

--- himan.py
+++ himan.py
@@ -5,14 +5,14 @@ class Database:
     file_name = input("Enter Database Name:")
 
 
-
-
+##### CREATING DATABASE
     def creating_data_base(self):
         if self.file_name is self.file_name:
             print("file exist")
         else:
             open(self.file_name, 'w+')
 
+#### CRATING TABLE
     def creating_table(self):
         try:
             con = lite.connect(self.file_name)
@@ -24,6 +24,7 @@ class Database:
             if con:
                 con.close()
 
+#### INSTERT DATA INTO TABLE
     def insert_data(self):
         try:
             con = lite.connect(self.file_name)
@@ -35,9 +36,7 @@ class Database:
             if con:
                 con.close()
 
-
-
-
+#### QUERY FROM DATABASE
 
     def database_query(self):
         try:
@@ -52,11 +51,45 @@ class Database:
                 con.close()
 
 
+class Operation(Database):
+#
+#  Memo: Create ERR class
+#
+    def start(self):
+        print("WELCOME IN HIMAN\n CHOSE OPTIONS \n 1.CREATE DATABASE\n 2.CREATE TABLE\n 3.ADD DATA TO TABLE\n 4.QUERY DATA\n\n\n")
+        choice = ""
+        base = Database()
+
+        while choice != "exit":
+            choice = input("Chose number : ")
+            if choice == '1':
+                try:
+                    base.creating_data_base()
+                except:
+                    print("ERROR003")
+
+            elif choice == '2':
+                try:
+                    base.creating_table()
+                except:
+                    print("ERROR003")
+            elif choice == '3':
+                try:
+                    base.insert_data()
+                except:
+                    print("ERROR003")
+            if choice == '4':
+                try:
+                    base.database_query()
+                except:
+                    print("ERROR003")
+
+            else:
+                quit()
 
 
 
-
-file1 = Database()
-
-file1.insert_data()
-file1.database_query()
+#run = Operation()
+#run.start()
+run = CratingTable()
+run.inputdata()
