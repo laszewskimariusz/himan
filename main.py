6fcafe12ac45be831e4aff9ab109650270baaa4f
Mariusz Laszewski <laszewskimariusz@gmail.com>
2015-06-12 20:44:30 +0100

    Fixed Login screen

--- main.py
+++ main.py
@@ -1,65 +1,85 @@
 from tkinter  import *
-import sys
+import sqlite3 as lite
 from hibase import *
 
-root = Tk()
-base = Database
-base.admin_logon
 
+class DatabaseOperation:
+    def admin_logon(self):
 
-class MenuOptions:                        ### ALL menu vairables
-    def Connection(self):
-        print("connected")
-
-    def Test(self):
-        print("dupa1")
-
-
-
-class UserLogin:
-    def main_login(self):
-        usr = StringVar()
-        paswd = StringVar()
-
-        Label(root, text="User").pack(side=TOP)
-        user = Entry(root, textvariable=usr).pack
-        user()
-
-        Label(root, text="Password").pack(side=TOP)
-        password = Entry(root, textvariable=paswd).pack
-        password()
-        log_button = Button(root, text=('ENTER'), command=self.auth).pack
-        log_button()
+        try:
+            self.con = lite.connect(r"test") ## na potrzeby testu przypisalem baze na sztywno
+            self.cur = self.con.cursor()
+            self.cur.execute('SELECT name, password FROM users')
+            self.data = self.cur.fetchall()
+            for row in self.data:
+                self.cur.close()
+        finally:
+            if self.con:
+                self.con.close()
+        usr = row[0]
+        paswd = row[1]
 
 
 
 
-class Menus(UserLogin):
-    menu = Menu(root)
-    root.config(menu=menu)
-    filemenu = Menu(menu)
-
-    def topmenu(self): #### Rozciagne menu file
-
-        Login = UserLogin()
-        self.menu.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="Login", command= Login.main_login)  ###### WIDGETS --> Login screen
+class Application(Frame, DatabaseOperation):
 
 
 
-class App(Frame, Menus):
-    def __init__(self, master=None):
-        top_menu = Menus()
-        top_menu.topmenu()
-
+    def __init__(self, master):
         Frame.__init__(self, master)
         self.pack()
+        self.login_widget()
+
+    def login_widget(self):
+        self.instruction = Label(self, text = "Password:")
+        self.instruction.grid(row = 0, column = 0, columnspan = 2, sticky = W)
+
+        self.password = Entry(self)
+        self.password.grid(row = 0, column = 1, sticky = W)
+
+
+        self.instruction = Label(self, text = "User:")
+        self.instruction.grid(row = 3, column = 0, columnspan = 2, sticky = W)
+
+        self.user = Entry(self)
+        self.user.grid(row = 3, column = 1, sticky = W)
 
 
 
-myapp = App()
-myapp.master.title("HiMan")
-myapp.master.maxsize(1600, 900)
-myapp.master.minsize(600, 400)
-myapp.master.wm_iconbitmap("icon.ico")
-myapp.mainloop()
+
+        self.submit_button = Button(self, text="Submit", command = self.reveal)
+        self.submit_button.grid(row = 4, column = 1, sticky = W)
+
+        self.text = Text(self, width = 35, height = 5, wrap = WORD)
+        self.text.grid(row = 5, column = 0, columnspan =2, sticky = W)
+
+    def reveal(self):
+        bz = DatabaseOperation()
+        bz.admin_logon()
+        password = self.password.get()
+        user = self.user.get()
+        zip(password, user)
+        content = zip
+        for row in bz.data:
+            usr = row[0]
+            pswd = row[1]
+            zip(usr, pswd)
+
+        if content == (zip):
+            message = ("Dobre haslo")
+        else:
+            message = ("dupa")
+        self.text.insert(0.0, message)
+
+
+
+
+
+root = Tk()
+root.geometry("800x600")
+app = Application(root)
+#### Na potrzeby testow
+
+
+root.mainloop()
